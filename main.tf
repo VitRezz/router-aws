7ad897855002848147ea214db8edc0469b9ed522
root <root@VITOR-PC.localdomain>
2022-09-29 22:35:36 -0300

    aws-router

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "vitor-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "sa-east-1"
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "vitorrez"
+      managed-by = "terraform"
+    }
+  }
+}
